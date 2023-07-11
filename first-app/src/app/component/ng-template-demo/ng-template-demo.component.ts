90d6a2c6ef2d67463418c51578744d0d6eeb2509
charchil08 <er.charchilkajaliya@gmail.com>
2023-07-11 16:19:06 +0530

    ngClass

--- first-app/src/app/component/ng-template-demo/ng-template-demo.component.ts
+++ first-app/src/app/component/ng-template-demo/ng-template-demo.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface IPassenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+}
+
 @Component({
   selector: 'app-ng-template-demo',
   templateUrl: './ng-template-demo.component.html',
@@ -7,9 +13,41 @@ import { Component } from '@angular/core';
 })
 export class NgTemplateDemoComponent {
   public searchQuery: string;
-
+  public passengers: IPassenger[];
   constructor() {
     this.searchQuery = '';
+    this.passengers = [
+      {
+        id: 1,
+        fullname: 'Charchil Kajaliya',
+        checkedIn: true
+      },
+      {
+        id: 2,
+        fullname: 'Rahul Kajaliya',
+        checkedIn: false
+      },
+      {
+        id: 3,
+        fullname: 'Raj Kajaliya',
+        checkedIn: true
+      },
+      {
+        id: 4,
+        fullname: 'Ravi Kajaliya',
+        checkedIn: false
+      },
+      {
+        id: 5,
+        fullname: 'Rajesh Kajaliya',
+        checkedIn: true
+      },
+      {
+        id: 6,
+        fullname: 'Rakesh Kajaliya',
+        checkedIn: false
+      },
+    ]
   }
 
   handleChange(event: any): void {
