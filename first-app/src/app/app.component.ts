c3a2e38ef1b5ad37586cd918145daf05b43bfbf3
charchil08 <er.charchilkajaliya@gmail.com>
2023-07-11 12:20:00 +0530

    property binding

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -6,14 +6,17 @@ import { Component } from '@angular/core';
   <div>
     <h1>{{ title }}</h1>
     <h2>App component works!</h2>
+    <input type="text" [value]=name />
   </div>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title: string;
+  name: string;
 
   constructor() {
     this.title = 'First App';
+    this.name = 'Charchil Kajaliya';
   }
 }
