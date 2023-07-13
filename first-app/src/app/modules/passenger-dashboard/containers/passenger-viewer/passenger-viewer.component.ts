e1e7a86c818bd620ea5f4e1ae62066a4934f9c87
charchil08 <er.charchilkajaliya@gmail.com>
2023-07-13 16:39:54 +0530

    viewer component

---
+++ first-app/src/app/modules/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passenger-viewer',
+  templateUrl: './passenger-viewer.component.html',
+  styleUrls: ['./passenger-viewer.component.scss']
+})
+export class PassengerViewerComponent {
+
+}
