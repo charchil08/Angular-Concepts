f685738a9e80a1b3e307132f69ef23d73b4e9670
charchil08 <er.charchilkajaliya@gmail.com>
2023-07-14 15:56:53 +0530

    pipe and swithmap to get data of single passenger

--- first-app/src/app/modules/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ first-app/src/app/modules/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IPassenger } from '../../models/passenger.interface';
 import { PassengerDashboardService } from '../../services/passenger-dashboard.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-passenger-viewer',
@@ -11,11 +13,13 @@ export class PassengerViewerComponent implements OnInit {
 
   passenger!: IPassenger;
 
-  constructor(private _service: PassengerDashboardService) { }
+  constructor(private _service: PassengerDashboardService, private _router: Router, private _route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this._service
-      .getById(1)
+    this._route.params
+      .pipe(
+        switchMap((data: Params) => this._service.getById(+data['id']))
+      )
       .subscribe((data: IPassenger) => this.passenger = data);
   }
 
