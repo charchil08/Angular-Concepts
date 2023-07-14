1d997e67f1b8007cb6ab5fbb5f98c9165afee5e5
charchil08 <er.charchilkajaliya@gmail.com>
2023-07-14 13:00:28 +0530

    dynamic-navigation

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
+import { INav } from './shared/interface/nav.interface';
 
 @Component({
   selector: 'app-root',
   template: `
+    <nav class="nav">
+      <a *ngFor="let item of nav" [routerLink]="item.link" routerLinkActive="active" [routerLinkActiveOptions]="{exact:item.exact}" >{{item.name}}</a>
+    </nav>
     <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string;
-  name: string;
+
+  Nav!: INav[];
 
   constructor() {
-    this.title = 'First App';
-    this.name = 'Charchil Kajaliya';
+    this.Nav = [
+      {
+        link: '/',
+        name: 'Home',
+        exact: true
+      },
+      {
+        link: '/oops',
+        name: 'OOPS',
+        exact: false
+      }
+    ]
   }
 }
