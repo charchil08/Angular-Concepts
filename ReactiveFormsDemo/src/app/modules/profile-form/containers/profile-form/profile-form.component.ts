0dd472595fcb1efeaa4ea951d57278bac64c972a
charchil08 <er.charchilkajaliya@gmail.com>
2023-07-28 18:34:59 +0530

    create:component

--- ReactiveFormsDemo/src/app/modules/profile-form/containers/profile-form/profile-form.component.ts
+++ ReactiveFormsDemo/src/app/modules/profile-form/containers/profile-form/profile-form.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-form',
   templateUrl: './profile-form.component.html',
   styleUrls: ['./profile-form.component.scss']
 })
-export class ProfileFormComponent {
 
+export class ProfileFormComponent {
+  constructor(private _fb: FormBuilder) { }
+
+  profileForm = this._fb.group({
+    firstName: [''],
+    lastName: [''],
+    email: [''],
+    phone: [''],
+    address: this._fb.group({
+      street: [''],
+      city: ['']
+    })
+  });
 }
+
